e22028e10a1015463faa3a076f8e688c12579032
taimourz <taimour.rkt@gmail.com>
2023-02-25 16:05:05 +0500

    added a button that shows paragraph. observed the changes in realDOM v/s virtualDOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React , {useState} from 'react';
+import Button from "./components/UI/Button/Button";
 
 import './App.css';
 
 function App() {
+  
+  const [showParagraph, setShowParagraph] = useState()
+ 
+  console.log('APP RUNNING')
+
+  const toggleParagraphHandler = () =>{
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>i am taimour</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
 }
